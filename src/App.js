e04f232f7c7e0967a16701ef2a0fe19cfd9a6f66
Balaji <balaji@revidd.com>
2022-12-12 14:51:41 +0530

    env setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Wip from "./wip.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <h1 className="text-center text-2xl">This is Balaji's Portfolio</h1>
+        <img src={Wip} alt="Under maintenance"/>
+      </div>
+    
   );
 }
 
